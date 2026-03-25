895373d456c714181c855305f760ba6ed2dfc50b
sidec4540-ops <sidec4540@gmail.com>
2026-03-25 12:58:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ INITIAL_BLACKLIST = [
     "@virusgift", "@portalsrelayer", "@lucha", "@snoopdogg", "@snoop",
     "@ufc", "@Tonnel_Network_bot", "@midasdep", "@portalsreceive", "@nftgiftbot", 
     "@GiftDrop_Warehouse", "@trade_relayer", "@rolls_transfer", "@GiftsToPortals", 
-    "@gemsrelayer", "@GiftDeposit", "@depgifts", "@telegram", "@gbrelayer"
+    "@gemsrelayer", "@GiftDeposit", "@depgifts", "@telegram", "@gbrelayer", "@match_money_gifts"
 ]
 
 async def init_blacklist_db():
